991336920794dfefb780f56559172719a8c0b2c5
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-29 01:59:42 +0700

    wiring up an interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { AuthHttpInterceptor } from './service/interceptor/auth/auth-http-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withInterceptors([AuthHttpInterceptor]), withFetch()),
   ],
 };
--- src/app/service/interceptor/auth/auth-http-interceptor.ts
+++ src/app/service/interceptor/auth/auth-http-interceptor.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  HttpEvent,
-  HttpInterceptor,
-  HttpHandler,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpEvent, HttpRequest, HttpHandlerFn } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
-export class AuthHttpInterceptor implements HttpInterceptor {
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {}
-}
+export const AuthHttpInterceptor = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<any>> => {
+  console.log(req);
+  return next(req);
+};

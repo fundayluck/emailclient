a346604e062150a852978f6e3cc8c9bbe8187069
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-04-30 14:32:30 +0700

    inbox component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,15 @@ import { Routes } from '@angular/router';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SignoutComponent } from './auth/signout/signout.component';
+import { HomeComponent } from './inbox/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'signout', component: SignoutComponent },
+  {
+    path: 'inbox',
+    loadComponent: () =>
+      import('./inbox/home/home.component').then((m) => m.HomeComponent),
+  },
 ];

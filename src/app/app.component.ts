94be2541f3cb8a8149cf4f370d1e43bf585cf297
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 23:07:19 +0700

    protected if authentication is true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { AuthService } from './service/auth.service';
 import { CommonModule } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
@@ -14,11 +19,17 @@ import { BehaviorSubject } from 'rxjs';
 export class AppComponent {
   signedIn$!: BehaviorSubject<boolean | null>;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.signedIn$ = this.authService.signedIn$;
+    console.log(this.signedIn$.getValue());
   }
 
   ngOnInit() {
-    this.authService.checkAuth().subscribe(() => {});
+    this.authService.checkAuth().subscribe((value) => {
+      if (value?.authenticated) {
+        this.router.navigateByUrl('/inbox');
+        return;
+      }
+    });
   }
 }

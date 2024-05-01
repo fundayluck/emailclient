8521b292d7cf80f14091c6570c979ec0ed6a1b13
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 23:26:06 +0700

    some more modal implementation

--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { EmailIndexComponent } from '../email-index/email-index.component';
 import { RouterOutlet } from '@angular/router';
 import { NotFoundComponent } from '../not-found/not-found.component';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, EmailIndexComponent],
+  imports: [RouterOutlet, EmailIndexComponent, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

b2c7322f355c78acf2db3c8769b1f81f00d0e50b
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 23:45:32 +0700

    providing a form default values

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ModalComponent } from '../../shared/modal/modal.component';
 import { EmailFormComponent } from '../email-form/email-form.component';
+import { Email } from '../../service/response/email/response-email';
 
 @Component({
   selector: 'app-email-create',
@@ -12,4 +13,16 @@ import { EmailFormComponent } from '../email-form/email-form.component';
 })
 export class EmailCreateComponent {
   showModal: boolean = false;
+  email: Email;
+
+  constructor() {
+    this.email = {
+      id: '',
+      to: '',
+      subject: '',
+      html: '',
+      text: '',
+      from: 'test11@example.com',
+    };
+  }
 }
--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Email } from '../../service/response/email/response-email';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-email-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './email-form.component.html',
-  styleUrl: './email-form.component.css'
+  styleUrl: './email-form.component.css',
 })
 export class EmailFormComponent {
+  emailForm!: FormGroup;
+  @Input() email!: Email;
 
+  ngOnInit() {
+    const { subject, from, to, text } = this.email;
+
+    this.emailForm = new FormGroup({
+      to: new FormControl(to),
+      subject: new FormControl(subject),
+      from: new FormControl(from),
+      text: new FormControl(text),
+    });
+  }
 }

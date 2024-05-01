2e175bc1845c5b9aae226c0e0755b5ebd1b2b045
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 14:19:00 +0700

    child route navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SignoutComponent } from './auth/signout/signout.component';
 import { AuthGuard } from './auth/auth.guard';
+import { PlaceholderComponent } from './inbox/placeholder/placeholder.component';
 
 export const routes: Routes = [
   { path: '', component: SigninComponent },
@@ -14,5 +15,21 @@ export const routes: Routes = [
     canActivate: [AuthGuard],
     loadComponent: () =>
       import('./inbox/home/home.component').then((m) => m.HomeComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./inbox/placeholder/placeholder.component').then(
+            (m) => m.PlaceholderComponent
+          ),
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./inbox/email-show/email-show.component').then(
+            (m) => m.EmailShowComponent
+          ),
+      },
+    ],
   },
 ];
--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { EmailIndexComponent } from '../email-index/email-index.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [EmailIndexComponent],
+  imports: [RouterOutlet, EmailIndexComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

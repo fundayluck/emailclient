81f6913a7e956787c51db52d34021ae3b8b48bd6
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 23:37:26 +0700

    toggling modal visibility

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { EmailFormComponent } from '../email-form/email-form.component';
 
 @Component({
   selector: 'app-email-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ModalComponent, EmailFormComponent],
   templateUrl: './email-create.component.html',
-  styleUrl: './email-create.component.css'
+  styleUrl: './email-create.component.css',
 })
 export class EmailCreateComponent {
-
+  showModal: boolean = false;
 }

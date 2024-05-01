4557fccb26e0c4b6915a2090f83e47cc1f99b03d
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 14:09:35 +0700

    rendering a list of emails

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { EmailService } from '../../service/email.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-email-index',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './email-index.component.html',
-  styleUrl: './email-index.component.css'
+  styleUrl: './email-index.component.css',
 })
 export class EmailIndexComponent {
+  emails: any = [];
 
+  constructor(private emailService: EmailService) {}
+
+  ngOnInit() {
+    this.emailService.getEmail().subscribe((emails) => {
+      this.emails = emails;
+    });
+  }
 }
--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { EmailIndexComponent } from '../email-index/email-index.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [EmailIndexComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}

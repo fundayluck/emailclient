acc853702df2bbb36a42a33771647138b1c2e5c6
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-01 23:30:07 +0700

    adding create email

--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -3,12 +3,22 @@ import { EmailIndexComponent } from '../email-index/email-index.component';
 import { RouterOutlet } from '@angular/router';
 import { NotFoundComponent } from '../not-found/not-found.component';
 import { ModalComponent } from '../../shared/modal/modal.component';
+import { EmailCreateComponent } from '../email-create/email-create.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, EmailIndexComponent, ModalComponent],
+  imports: [
+    RouterOutlet,
+    EmailIndexComponent,
+    ModalComponent,
+    EmailCreateComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  showModal() {
+    throw new Error('Method not implemented.');
+  }
+}

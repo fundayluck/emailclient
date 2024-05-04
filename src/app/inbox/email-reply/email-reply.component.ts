dd4dfa9eb0aa882b8ec34ff76a6f42ffc3122b58
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-04 16:19:46 +0700

    replying to emails

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { EmailFormComponent } from '../email-form/email-form.component';
+import { Email } from '../../service/response/email/response-email';
 
 @Component({
   selector: 'app-email-reply',
   standalone: true,
-  imports: [],
+  imports: [ModalComponent, EmailFormComponent],
   templateUrl: './email-reply.component.html',
-  styleUrl: './email-reply.component.css'
+  styleUrl: './email-reply.component.css',
 })
 export class EmailReplyComponent {
+  showModal = false;
+  email!: Email;
 
+  constructor() {}
+
+  onSubmit(email: Email) {}
 }
